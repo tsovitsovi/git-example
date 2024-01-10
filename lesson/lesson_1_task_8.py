c36de6e29b826c02ed775f6e754747b43b307e49
Tsovinar <tsovinar.hakobya@mail.ru>
2024-01-10 21:38:47 +0300

    изменение

---
+++ lesson/lesson_1_task_8.py
@@ -0,0 +1,36 @@
+
+def func(num):
+    print(num)
+   
+func(8)
+func(8)
+func(0)
+func(0)
+func(5)
+func(5)
+func(5)
+func(3)
+func(5)
+func(3)
+func(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson/lesson_1task_7.py
@@ -0,0 +1,33 @@
+def funcA():
+    print(1)
+def funcB():
+    print(2)
+def funcC():
+    print(3)
+def funcD():
+    print(4)
+def funcE():
+    print(5)
+def funcF():
+    print(6)
+def funcG():
+    print(7)
+def funcH():
+    print(8)
+def funcI():
+    print(9)
+def funcJ():
+    print(0)
+
+
+funcH()
+funcH()
+funcJ()
+funcJ()
+funcE()
+funcE()
+funcE()
+funcC()
+funcE()
+funcC()
+funcE()
