6468c2efd4a18cff5854ec2eea8505034443a175
Tsovinar <tsovinar.hakobyan@mail.ru>
2024-03-13 15:21:46 +0300

    Изменеие

---
+++ lesson/lesson_3/user.py
@@ -0,0 +1,10 @@
+class User:
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        print(first_name)
+        print(last_name)
+        print(first_name,  last_name)
+        
+user=User("Tsovinar", "Hakobyan")
