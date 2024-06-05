c9199992059674cf9d7492bc0e9d743d1be08ad5
Tsovinar <tsovinar.hakobyan@mail.ru>
2024-06-05 08:48:02 +0300

    Изменеия

--- lesson/lesson_4/test_string_utils.py
+++ lesson/lesson_4/test_string_utils.py
@@ -1,7 +1,33 @@
 from string_utils import StringUtils
-
+import pytest
 
 def test_capitilize():
     srting_utilitis = StringUtils()
     res= srting_utilitis.capitilize(string= "skypro")
     assert res == "Skypro"
+
+def test_capitilize_nonText():
+    srting_utilitis = StringUtils()
+    res= srting_utilitis.capitilize(string= "")
+    assert res == ""
+
+
+def test_trim():
+    srting_utilitis = StringUtils()
+    res= srting_utilitis.trim(string= "   skypro")
+    assert res == "skypro"
+
+
+
+
+
+
+def test_to_list():
+    srting_utilitis = StringUtils()
+    res= srting_utilitis.to_list(string= "a,b,c,d")
+    assert res == ["a", "b", "c", "d"]
+
+def test_to_list():
+    srting_utilitis = StringUtils()
+    res= srting_utilitis.to_list(string= "a,b,c,d")
+    assert res == ["a", "b", "c", "d"]
